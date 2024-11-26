ddf381155e6d2f6a9e4f40798b13740579b84863
JuanitoDavid <jumorenoma@unal.edu.co>
2024-11-26 15:50:16 -0500

    Add files via upload

---
+++ Ejemplo1.java
@@ -0,0 +1,58 @@
+package ejemplo1;
+
+import java.util.Scanner;
+
+
+
+//Author: Nestor German Bolivar, Diego Francisco Mendez, Juan David Moreno
+//Abstract: This code makes you spontaneously combust
+
+public class Ejemplo1 {
+    
+    public static void main(String[] args) {
+        
+        
+        Ejemplo1.menu();
+ 
+        Cliente cliente = new Cliente("Hola",1);
+        
+        System.out.println(cliente.getNombre());
+        System.out.println("weee");
+
+        System.out.println("Hola");
+        
+    }
+        
+    
+    public static void registro(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        String nombre = s.nextLine();
+        System.out.println("Ingrese su núero de cedula");
+        int cedula = Integer.parseInt(s.nextLine());
+        
+        
+        System.out.println("");
+        
+    }
+    
+    public static void menu(){
+        Scanner s = new Scanner(System.in);
+        boolean check = true;
+        while(check == true){
+            System.out.println("Elige una opción: ");
+            System.out.println("1: Consignar");
+            System.out.println("2: Extraer");
+            int option= s.nextInt();
+            if (option==1) {
+                System.out.println("Hola XD");
+            } else if (option == 2){
+                System.out.println("Queda tan lejos que el agua llega pidiendo jugo");
+            }else{
+                System.out.println("Fin");
+                check = false;
+            }
+        }
+    }
+    
+}
