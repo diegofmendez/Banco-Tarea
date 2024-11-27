239334b7e809062c91fd4c60af4707d24cbe35bf
diegofmendez <146147741+diegofmendez@users.noreply.github.com>
2024-11-26 23:56:58 -0500

    Add files via upload
    new

--- src/ejemplo1/Ejemplo1.java
+++ src/ejemplo1/Ejemplo1.java
@@ -2,57 +2,52 @@ package ejemplo1;
 
 import java.util.Scanner;
 
-
-
-//Author: Nestor German Bolivar, Diego Francisco Mendez, Juan David Moreno
-//Abstract: This code makes you spontaneously combust
-
+//Author: Nestor German Bolivar, Diego Francisco Mendez
+//Abstract: This code is a very simplified version of a bank software
+ 
 public class Ejemplo1 {
     
     public static void main(String[] args) {
         
+        Cliente cliente = Ejemplo1.registro();
+        Cuenta cuenta = new Cuenta(cliente);
+        Ejemplo1.menu(cuenta, cliente);
         
-        Ejemplo1.menu();
- 
-        Cliente cliente = new Cliente("Hola",1);
-        
-        System.out.println(cliente.getNombre());
-        System.out.println("weee");
-
-        System.out.println("Hola");
-        
-    }
-        
-    
-    public static void registro(){
+    }   
+    public static Cliente registro(){
         Scanner s = new Scanner(System.in);
-        System.out.println("Ingrese su nombre");
+        System.out.println("Creación de cuenta nueva:");
+        System.out.println("Ingresa tu nombre completo");
         String nombre = s.nextLine();
-        System.out.println("Ingrese su núero de cedula");
+        System.out.println("Ingresa tu numero de cedula");
         int cedula = Integer.parseInt(s.nextLine());
-        
-        
-        System.out.println("");
-        
-    }
+        return new Cliente(nombre, cedula);
+        }
     
-    public static void menu(){
+    public static void menu(Cuenta cuenta, Cliente cliente){
         Scanner s = new Scanner(System.in);
         boolean check = true;
-        while(check == true){
-            System.out.println("Elige una opción: ");
-            System.out.println("1: Consignar");
-            System.out.println("2: Extraer");
-            int option= s.nextInt();
-            if (option==1) {
-                System.out.println("Hola XD");
-            } else if (option == 2){
-                System.out.println("Queda tan");
-            }else{
-                System.out.println("Fin");
-                check = false;
+        System.out.println("Bienvenido");
+        while (check == true) {                
+            System.out.println("Elije una opción");
+            System.out.println("1: Mostrar saldo");
+            System.out.println("2: Hacer una consignación");
+            System.out.println("3: Retirar saldo");
+            System.out.println("Cualquier otro número: Salir");
+            int index = Integer.parseInt(s.nextLine());
+            if (index==1){
+                System.out.println(cliente.getNombre());
+                System.out.println("Saldo actual: "+cuenta.getSaldo());
+            } else if(index==2){
+                System.out.println("Ingrese monto a consignar(Decimales con punto)");
+                cuenta.consignacion(Double.parseDouble(s.nextLine()));
+                
+            } else if(index==3){
+                System.out.println("Ingrese monto a retirar(Decimales con punto)");
+                cuenta.retiro(Double.parseDouble(s.nextLine()));
+            } else{
+                check=false;
             }
         }
     }
-    
 }
